52a1c5a9a3922b631d0aae8c381ee7ae7b1eeba4
Joseph C Lawrence <joe@clickshape.com>
2023-01-19 16:29:18 +0200

    Tree options update

--- src/pages/tree.js
+++ src/pages/tree.js
@@ -41,6 +41,56 @@ class Tree extends React.Component {
             cursorItem: 0,
             highlightItem: 0,
             hasSelectorCheckbox: Enabled.True,
+            pageFitMode: primitives.common.PageFitMode.SelectionOnly,
+            items: treeData,
+            // annotations: lineagePaths,
+            templates: [getTaijiTreeTemplate()],
+            onItemRender: onTaijiTemplateRender,
+            defaultTemplateName: "taijiTreeTemplate",
+            cursorItem: 2,
+            linesWidth: 1,
+            linesColor: "black",
+            hasSelectorCheckbox: primitives.common.Enabled.False,
+            normalLevelShift: 20,
+            dotLevelShift: 20,
+            lineLevelShift: 20,
+            normalItemsInterval: 10,
+            dotItemsInterval: 10,
+            lineItemsInterval: 10,
+            highlightItem: true,
+            highlightLinesWidth: 6,
+            highlightLinesColor: "#ff0000",
+            showNeigboursConnectorsHighlighted: true,
+            neighboursSelectionMode: 1,
+            enableMatrixLayout: true,
+            maximumColumnsInMatrix: 4,
+            selectionPathMode: 1,
+            navigationMode: 0,
+            templates: [{
+                name: "taijiTreeTemplate",
+                itemSize: new primitives.common.Size(160, 200),
+                minimizedItemSize: new primitives.common.Size(15, 15),
+                onItemRender: ({ context: itemConfig }) => {
+                    const itemTitleColor = itemConfig.itemTitleColor != null ? itemConfig.itemTitleColor : Colors.RoyalBlue;
+                    const backgroundColor = itemConfig.itemTitleColor || primitives.common.Colors.RoyalBlue;
+
+                    return <div className=" bp-item bp-corner-all bt-item-frame" style={{ width: '159px', height: '199px' }}>
+                    <div className=" bp-item bp-corner-all bp-title-frame" style={{ top: '2px', left: '2px', width: '216px', height: '40px', overflow: 'hidden', backgroundColor: 'rgb(65, 105, 225)' }}>
+                        <div className=" bp-item bp-title" style={{ top: '2px', left: '2px', width: '208px', height: '18px', fontSize: '14px', overflow: 'hidden' }}>{itemConfig.name}
+                        </div>
+                        <div className=" bp-item bp-title" style={{ top: '22px', left: '2px', width: '208px', height: '18px', fontSize: '14px', overflow: 'hidden' }}>{itemConfig.chineseName}</div>
+                    </div>
+                    <div className=" bp-item bp-photo-frame" style={{ top: '36px', left: '2px', width: '50px', height: '60px', overflow: 'hidden' }}>
+                        <img src={itemConfig.image} alt={itemConfig.name} style={{ width: '50px', height: '60px' }}></img>
+                    </div>
+                    <div className=" bp-item" style={{ top: '46px', left: '56px', width: '162px', height: '18px', fontSize: '12px', overflow: 'hidden' }}>{itemConfig.birthYear}</div>
+                    <div className=" bp-item" style={{ top: '64px', left: '56px', width: '162px', height: '18px', fontSize: '12px', overflow: 'hidden' }}>{itemConfig.deathYear}</div>
+                    <div className=" bp-item" style={{ top: '62px', left: '56px', width: '162px', height: '36px', fontSize: '10px', overflow: 'hidden' }}>{itemConfig.generation}</div>
+                    <div className=" bp-item" style={{ top: '-20px', left: '3px', width: '208px', height: '20px', fontSize: '14px', textAlign: 'center', overflow: 'hidden' }}>
+                        {itemConfig.description}</div>
+                </div>;
+                },
+            }],
             items: players.map((player) => {
                 const plainTextDescription = documentToPlainTextString(
                     JSON.parse(player.shortBio && player.shortBio !== null && player.shortBio !== undefined ? player.shortBio?.raw : null)
