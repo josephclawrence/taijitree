7b784e2663d2ca2cdb70ce7b8b6e3e19d84e40a6
Joseph C Lawrence <joe@clickshape.com>
2023-01-31 13:11:35 +0200

    Test fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -102,8 +102,8 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           slug: player.slug,
           // previousPlayerSlug,
           // nextPlayerSlug,
-          parentsOfCurrent: player.parents.map(parent => parent.slug),
-          studentsOfCurrent: player.students.map(student => student.slug),
+          parentsOfCurrent: player.parents?.map(parent => parent.slug),
+          studentsOfCurrent: player.students?.map(student => student.slug),
         },
       })
     })
